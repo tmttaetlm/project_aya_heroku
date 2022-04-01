be3d9e50087e7764e1c4c9da54661640933e0c4a
tmttaetlm <tmttaetlm@gmail.com>
2022-04-01 10:26:38 +0000

    бот

--- project_aya/apps/main/send_to_bot.py
+++ project_aya/apps/main/send_to_bot.py
@@ -1,24 +1,30 @@
-import os
+import sys, os
 import django
-
+sys.path.append('/home/Yerdos/project_aya') 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project_aya.settings")
 django.setup()
+from django.conf import settings
 
 import telebot
 from datetime import datetime
-from .models import User, Message
+from main.models import User, Message
 
-bot = telebot.TeleBot('5299933627:AAFadtni2QPlSxeikWyTYNN-DukFGkm_KY0')
+bot = telebot.TeleBot(settings.TELEGRAM_TOKEN)
 
 now = datetime.now().strftime('%d%m_%H%M')
-msg = Message.objects.get(clue='on_time_msg|'+now)
-users = User.objects.exclude(role='Админ')
-admin = User.objects.filter(role='Админ')
+try:
+    msg = Message.objects.get(clue='on_time_msg|'+now)
+    users = User.objects.exclude(role='Админ')
+    admin = User.objects.filter(role='Админ')
 
-text = msg[0].text + f'\n\n<b>Сообщение создано и отправлено администратором. Если требуется ответ, напишите администратору @{admin[0][1]} напрямую</b>'
-for usr in users: bot.send_message(usr.chat_id, text, parse_mode = 'HTML')
+    print(f'Сообщение {msg.id} отправлено')
 
-msg.delete()
+    text = msg.text + f'\n\n<b>Сообщение создано и отправлено администратором. Если требуется ответ, напишите администратору @{admin[0].user} напрямую</b>'
+    for usr in users: bot.send_message(usr.chat_id, text, parse_mode = 'HTML')
 
-bot.delete_message(admin[0].chat_id, admin[0].msg_id)
-bot.send_message(admin[0].chat_id, f'Сообщение, запланированное на {now[0:2]}.{now[2:4]} {now[5:7]}:{now[7:9]} отправлено всем пользователям бота')
+    msg.delete()
+
+    bot.delete_message(admin[0].chat_id, admin[0].msg_id)
+    bot.send_message(admin[0].chat_id, f'Сообщение, запланированное на {now[0:2]}.{now[2:4]} {now[5:7]}:{now[7:9]} отправлено всем пользователям бота')
+except Message.DoesNotExist:
+    print('Для отправки ничего не найдено')
