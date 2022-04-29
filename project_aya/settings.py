72d948069e9e8337ed1c646834f4ee947dea2466
Medet <turegeldin_m@kst.nis.edu.kz>
2022-04-29 11:32:12 +0600

    heroku

--- project_aya/settings.py
+++ project_aya/settings.py
@@ -134,10 +134,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '/static/'),
-)
+
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
