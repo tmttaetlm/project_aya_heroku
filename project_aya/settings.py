dc1907ff8918985835d1687a7a3277d5a935136f
Medet <turegeldin_m@kst.nis.edu.kz>
2022-04-29 12:30:16 +0600

    heroku

--- project_aya/settings.py
+++ project_aya/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
